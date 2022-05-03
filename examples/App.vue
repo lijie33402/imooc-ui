2288fddb2f86d135d5d8882f330745b8adb486e4
lijie33402 <823997187@qq.com>
2022-05-03 20:58:27 +0800

    编写示例卡片组件

--- examples/App.vue
+++ examples/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Demo />
+    <m-card imgSrc="moocui.png" summary="从零搭建vue组件库">
+      <template v-slot:footer>
+        <div class="footer-wrapper">
+          <div class="level">中级 - 528人报名</div>
+          <div class="price">399</div>
+        </div>
+      </template>
+    </m-card>
   </div>
 </template>
 
@@ -22,5 +30,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.footer-wrapper {
+  display: flex;
+  padding: 10px;
+  justify-content: space-between;
+  font-size: 12px;
+  color: gainsboro;
 }
 </style>
--- examples/main.js
+++ examples/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import '../components/css/demo.scss'
+import '../components/css/card.scss'
 import Demo from '../components/lib/demo/index'
+import Card from '../components/lib/card/index'
+
 Vue.use(Demo) // Demo.install ---> Vue.component(Demo.name, Demo)
+Vue.use(Card)
 Vue.config.productionTip = false
 
 new Vue({
