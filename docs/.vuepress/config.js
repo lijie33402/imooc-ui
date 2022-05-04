e802076b4d3db5bb2cf57dfd572dc0670839d81b
lijie33402 <823997187@qq.com>
2022-05-05 00:25:03 +0800

    feat: 完善文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  base: '/imooc-ui',
+  base: '/imooc-ui/',
   themeConfig: {
+    nav: [{
+      text: 'Home',
+      link: '/'
+    },{
+      text: 'Github',
+      link: 'https://github.com/lijie33402/imooc-ui'
+    },{
+      text: 'Npm',
+      link: 'https://www.npmjs.com/package/imooc-ui'
+    },],
     sidebar: [
       '/',
       '/componentsDocs/card',
